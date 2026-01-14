17d7d8c43f85b16c1c615e0a20275c133915e13c
ChakornK <chakornk2007@gmail.com>
2026-01-14 15:55:04 -0800

    feat: basic bottom sheet for category selector

---
+++ src/components/BottomSheet.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Teleport } from 'vue';
+
+const { open, onClose } = defineProps<{
+  open: boolean;
+  onClose: () => void;
+}>();
+
+</script>
+
+<template>
+  <Teleport to="body">
+    <div class="fixed inset-0 z-50 flex items-end justify-center" v-if="open">
+      <div class="absolute inset-0 bg-black/50" @click="onClose"></div>
+      <div class="absolute bottom-0 top-8 w-full max-w-xl rounded-t-xl bg-gray-800 text-white shadow-lg">
+        <slot></slot>
+      </div>
+    </div>
+  </Teleport>
+</template>
--- src/views/SetupView.vue
+++ src/views/SetupView.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import { HugeiconsIcon } from '@hugeicons/vue';
-import { ArrowRight01Icon, Book02Icon, Cancel01Icon, UserMultipleIcon } from '@hugeicons/core-free-icons';
+import { ArrowRight01Icon, Book02Icon, Cancel01Icon, Tick02Icon, UserMultipleIcon } from '@hugeicons/core-free-icons';
 import { ref, useTemplateRef } from 'vue';
+import BottomSheet from '@/components/BottomSheet.vue';
 
 const players = ref<string[]>([]);
 const newPlayerName = ref<string>('');
 
 const nameInputRef = useTemplateRef('name-input');
 const addPlayerRef = useTemplateRef('add-player-button');
+
+const categoryPickerOpen = ref(false);
 </script>
 
 <template>
   <div class="flex h-full w-full flex-col">
-    <div class="flex grow flex-col gap-2 p-4">
+    <div class="mx-auto flex max-w-xl grow flex-col gap-2 p-4">
       <h1 class="text-3xl font-bold">Imposter</h1>
       <p>All but one person knows the word. Everyone gives 1-word clues and tries to find who the imposter is.</p>
 
@@ -38,7 +41,7 @@ const addPlayerRef = useTemplateRef('add-player-button');
           }">Add</button>
         </div>
 
-        <div class="flex flex-col gap-1">
+        <div class="flex flex-col gap-2">
           <div v-for="(player, index) in players" :key="index"
             class="flex items-center gap-2 rounded-lg bg-gray-700 px-4 py-2">
             <span class="grow">{{ player }}</span>
@@ -59,16 +62,39 @@ const addPlayerRef = useTemplateRef('add-player-button');
           </p>
         </div>
 
-        <button class="flex items-center justify-between rounded-lg border border-gray-700 bg-gray-900 px-4 py-2">
+        <button class="flex items-center justify-between rounded-lg border border-gray-700 bg-gray-900 px-4 py-2"
+          @click="() => categoryPickerOpen = true">
           <p>Choose categories</p>
           <HugeiconsIcon :icon="ArrowRight01Icon" size="20" />
         </button>
+        <BottomSheet :open="categoryPickerOpen" :onClose="() => categoryPickerOpen = false">
+          <div class="flex max-h-full flex-col">
+            <div class="flex justify-between p-4">
+              <h2 class="text-xl font-bold">Categories</h2>
+              <button class="flex h-8 w-8 items-center justify-center rounded hover:bg-gray-700"
+                @click="() => categoryPickerOpen = false">
+                <HugeiconsIcon :icon="Cancel01Icon" size="20" />
+              </button>
+            </div>
+            <div class="flex grow flex-col items-stretch gap-2 overflow-scroll px-4 pb-4">
+              <button v-for="i in Array.from({ length: 100 }).map((_, i) => i)" :key="i"
+                class="flex gap-2 rounded-lg border border-gray-700 bg-gray-700 p-2">
+                <div>
+                  <HugeiconsIcon :icon="Tick02Icon" size="16" />
+                </div>
+                <p>{{ i }}</p>
+              </button>
+            </div>
+          </div>
+        </BottomSheet>
       </div>
     </div>
 
 
-    <div class="h-20 shrink-0 border-t border-gray-700 p-4">
-      <button class="btn h-full w-full">Start</button>
+    <div class="h-20 shrink-0 border-t border-gray-700">
+      <div class="mx-auto flex h-full max-w-xl items-center p-4">
+        <button class="btn h-full w-full">Start</button>
+      </div>
     </div>
   </div>
 </template>
