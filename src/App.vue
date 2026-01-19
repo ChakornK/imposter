fb83135fd8f757f92719e9245fa7236189fe57f6
ChakornK <chakornk2007@gmail.com>
2026-01-19 02:51:47 -0800

    feat: override browser navigation

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,21 @@
 import GameView from "./views/GameView.vue"
 import SetupView from "./views/SetupView.vue"
 import { store } from "./store"
+import { onMounted, onUnmounted } from "vue"
+
+const handlePopState = () => {
+  if (store.view === 'game') {
+    store.view = 'setup';
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('popstate', handlePopState);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('popstate', handlePopState);
+});
 </script>
 
 <template>
