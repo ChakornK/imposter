2aa61a8eb08e5a44cdeff411edb803c9776596b6
ChakornK <chakornk2007@gmail.com>
2026-01-19 17:57:08 -0800

    feat: bottom sheet animation

--- src/components/BottomSheet.vue
+++ src/components/BottomSheet.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { AnimatePresence, motion, type VariantType } from 'motion-v';
 import { Teleport } from 'vue';
 
 const { open, onClose } = defineProps<{
@@ -6,15 +7,32 @@ const { open, onClose } = defineProps<{
   onClose: () => void;
 }>();
 
+const overlayVariants: Record<string, VariantType> = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 }
+}
+const sheetVariants: Record<string, VariantType> = {
+  visible: {
+    y: 0,
+    transition: { ease: "easeInOut", duration: 0.15 }
+  },
+  hidden: {
+    y: '100%',
+    transition: { ease: "anticipate" }
+  }
+}
 </script>
 
 <template>
   <Teleport to="body">
-    <div class="fixed inset-0 z-50 flex items-end justify-center" v-if="open">
-      <div class="absolute inset-0 bg-black/50" @click="onClose"></div>
-      <div class="absolute bottom-0 top-8 w-full max-w-xl rounded-t-xl bg-gray-800 text-white shadow-lg">
+    <AnimatePresence>
+      <motion.div v-if="open" class="fixed inset-0 bg-black/50" @click="onClose" :variants="overlayVariants"
+        initial="hidden" animate="visible" exit="hidden"></motion.div>
+      <motion.div v-if="open"
+        class="border-b-128 fixed bottom-0 left-1/2 top-8 -mb-32 w-full max-w-xl -translate-x-1/2 rounded-t-xl border-gray-800 bg-gray-800 text-white shadow-lg"
+        :variants="sheetVariants" initial="hidden" animate="visible" exit="hidden">
         <slot></slot>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   </Teleport>
 </template>
